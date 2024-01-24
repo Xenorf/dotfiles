7aa8f6cb70ebbed55fea1ac95266d5c5260e28e1
Xenorf <xenorf@gmail.com>
2024-01-24 22:51:11 +0100

    usb notification

---
+++ .local/bin/usbmonitor.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+if [ -z "$1" ] || [ "$1" != "plug" ] && [ "$1" != "unplug" ]; then
+	echo "Usage: $0 <plug|unplug>"
+	exit 1
+fi
+icon="$HOME/.config/resources/icons/usbkey.png"
+
+usb_action="$1"
+echo "oui $usb_action" >>/home/louis/test.log
+
+case "$usb_action" in
+"plug")
+	dunstify "USB key plugged in"
+	DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus dunstify -I $icon "USB key plugged in"
+	;;
+"unplug")
+	dunstify "USB key unplugged"
+	DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus dunstify -I $icon "USB key unplugged"
+	;;
+esac
